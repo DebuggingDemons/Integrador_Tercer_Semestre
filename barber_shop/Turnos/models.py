57a4eb3da21bf25cfb70b428d8b863680a4ca05f
Sebastián Galván <sebastiangalvan.ar@gmail.com>
2023-06-14 21:57:50 -0300

    [REF] Modelo de turnos

--- barber_shop/Turnos/models.py
+++ barber_shop/Turnos/models.py
@@ -1,21 +1,20 @@
 from django.db import models
 
+from Usuarios.models import Usuario
+
 # Create your models here.
 class Turno(models.Model):
-
-    idTurno = models.IntegerField
-    idCliente = models.ForeignKey(Usuarios, on_delete=models. CASCADE)
-    idProf = models.ForeignKey(Usuarios, on_delete=models. CASCADE)
-    idServicio = models.ForeignKey(Servicios, on_delete=models. CASCADE)
+    id = models.IntegerField(primary_key=True, auto_created=True)
+    usuario = models.ForeignKey(Usuario, on_delete=models. CASCADE)
+    profesional = models.ForeignKey(Usuario, on_delete=models. CASCADE, related_name='turno_profesional', default=None)
+    # idServicio = models.ForeignKey(Servicios, on_delete=models. CASCADE)
     detalle = models.CharField(max_length=200)
-    fechaHora = models.DateTimeField
+    fechaHora = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
         return f'''
-            Id Turno: {self.idTurno},
-            Id Cliente: {self.idCliente},
-            Id Profesional: {self.idProf},
-            Id Servicio: {self.idServicio},
+            Id Cliente: {self.cliente},
+            Id Profesional: {self.profesional},
             Detalles:  {self.detalle},
             Fecha: {self.fechaHora}        
         '''
