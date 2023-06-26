5313875a006d51d0cd702f29a28ceba91abe5e1d
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-06-26 02:10:39 -0300

    [UPDATE] actualizacion model servicios

--- barber_shop/servicios/models.py
+++ barber_shop/servicios/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 class Servicios(models.Model):
     idServicio = models.AutoField(primary_key=True)
@@ -6,6 +7,10 @@ class Servicios(models.Model):
     descripcion = models.CharField(max_length=200, blank=False)
     precio = models.DecimalField(max_length=10, blank=False, decimal_places=2, max_digits=10,
                                  validators=[MinValueValidator(0.01)])
+
+class Meta:
+    db_table = 'servicios'
+
 
     def __str__(self) -> str:
             return f'''
