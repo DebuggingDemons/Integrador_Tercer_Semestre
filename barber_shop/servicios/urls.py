52102f02e7ae0d5d6b644e34eca4221b2289ca6c
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-06-25 22:34:37 -0300

    [ADD] funcion servicios - registro url servicios

---
+++ barber_shop/servicios/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from servicios import views
+from django.contrib.auth.views import LogoutView
+
+
+urlpatterns = [
+    # (temlate, vista, nombre dentro de un html)
+    path('servicios/', view=views.servicios, name='servicios'),  
+]
--- barber_shop/servicios/views.py
+++ barber_shop/servicios/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def servicios(request):
+    form = formularioDeLogin
+    return render(request, 'servicios/servicios.html', {'form':form})
+
+    
