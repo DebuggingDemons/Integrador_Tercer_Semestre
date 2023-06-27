a1c48b164e50452e2557aac1e8d2878e29f81ce3
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-06-27 04:05:29 -0300

    [ADD] variable corregida

--- barber_shop/servicios/views.py
+++ barber_shop/servicios/views.py
@@ -3,28 +3,39 @@ from .models import Servicios
 
 # Create your views here.
 
-def agregarServicio(request):
+def agregarServicio(request): # Post
     if request.POST:
         nombre = request.POST['nombre']
         precio = request.POST['precio']
         descripcion = request.POST['descripcion']
 
-        servicio = Servicios(nombre=nombre, precio=precio, descripcion=descripcion)
-        servicio.save()
+        servicios = Servicios(nombre=nombre, precio=precio, descripcion=descripcion)
+        servicios.save()
     return render(request, 'servicios.html')
 
 
-def mostrarServicio(request):
+def mostrarServicio(request): #Get
     servicios = Servicios.objects.all()
     return render(request, 'mostrar_servicio.html', {'servicios': servicios})
 
 
 
-
-def eliminarServicio(request, id):
+def eliminarServicio(request, id): #Del
     servicios = get_object_or_404(Servicios, idServicio=id)
 
     if request.method == 'POST':
         servicios.delete()
         return redirect('mostrar_servicio')
+    return render(request, 'mostrar_servicio.html', {'servicios': servicios})
+
+
+def modificarServicio(request): # Put
+    if request.POST:
+        nombre = request.POST['nombre']
+        precio = request.POST['precio']
+        descripcion = request.POST['descripcion']
+
+        servicios = Servicios(nombre=nombre, precio=precio, descripcion=descripcion)
+        servicios.save()
+        return render(request, 'mostrar_servicio.html', {'servicios': servicios})
     return render(request, 'mostrar_servicio.html', {'servicios': servicios})
