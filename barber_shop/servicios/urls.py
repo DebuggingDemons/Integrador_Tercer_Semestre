658be504461f91dcfb7ab8421937e7c826d85b82
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-06-26 23:37:39 -0300

    [ADD] html vinculado y verbo get funcionando

--- barber_shop/servicios/urls.py
+++ barber_shop/servicios/urls.py
@@ -5,5 +5,6 @@ from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     # (temlate, vista, nombre dentro de un html)
-    path('servicios/', view=views.agregarServicio, name='servicios'),  
+    path('servicios/', view=views.agregarServicio, name='servicios'),
+    path('servicios/mostrar_servicio', view=views.mostrarServicio, name='mostrar_servicio'),
 ]
--- barber_shop/servicios/views.py
+++ barber_shop/servicios/views.py
@@ -1,16 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Servicios
-
 
 # Create your views here.
 
 def agregarServicio(request):
-  if request.POST: 
-    nombre =  request.POST['nombre']
-    precio =  request.POST['precio']
-    descripcion =  request.POST['descripcion']
+    if request.POST:
+        nombre = request.POST['nombre']
+        precio = request.POST['precio']
+        descripcion = request.POST['descripcion']
 
-    servicio = Servicios(nombre=nombre, precio=precio, descripcion=descripcion)
-    servicio.save()
-  return render(request,'servicios.html')
+        servicio = Servicios(nombre=nombre, precio=precio, descripcion=descripcion)
+        servicio.save()
+    return render(request, 'servicios.html')
 
+
+def mostrarServicio(request):
+    servicios = Servicios.objects.all()
+    return render(request, 'mostrar_servicio.html', {'servicios': servicios})
